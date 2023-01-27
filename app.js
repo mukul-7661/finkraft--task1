8dd24f0969fe8aee24081191023660be9fd3851d
Mukul yadav <mukul7661@gmail.com>
2023-01-27 17:05:11 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+function LCSSubStr(X, Y, m, n) {
+  var LCStuff = Array(m + 1)
+    .fill()
+    .map(() => Array(n + 1).fill(0));
+
+  var result = 0;
+
+  for (var i = 0; i <= m; i++) {
+    for (var j = 0; j <= n; j++) {
+      if (i == 0 || j == 0) {
+        LCStuff[i][j] = 0;
+      } else if (X[i - 1] == Y[j - 1]) {
+        LCStuff[i][j] = LCStuff[i - 1][j - 1] + 1;
+        result = Math.max(result, LCStuff[i][j]);
+      } else {
+        LCStuff[i][j] = 0;
+      }
+    }
+  }
+
+  return result;
+}
